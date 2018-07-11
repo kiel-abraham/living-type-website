d86e74135bfcd4fe129a4ec77b7c8b2b9821ff83
kiel-abraham <kiel.abraham@icloud.com>
2018-07-11 19:52:21 +1000

    preview work

--- src/cms/preview-templates/customs-preview.js
+++ src/cms/preview-templates/customs-preview.js
@@ -2,23 +2,24 @@ import React from 'react';
 import Hero from '../../components/hero';
 import { Row, Col } from 'reactstrap';
 
-const CustomsPreview = ({ entry, widgetFor }) => (
-  <div>
-    <Hero
-      hero={entry.getIn(['data', 'hero'])}
-      heroFull={entry.getIn(['data', 'heroFull'])}
-      heroTitle={entry.getIn(['data', 'heroTitle'])}
-    />
+const CustomsPreview = ({ entry, widgetFor }) => {
+  const hero = entry.getIn(['data', 'hero']);
+  const heroFull = entry.getIn(['data', 'heroFull']);
+  const heroTitle = entry.getIn(['data', 'heroTitle'])
+  return (
+    <div>
+      <Hero heroData={hero, heroFull, heroTitle} />
 
-    <section className="container">
-      <Row>
-        <Col>
-          <h1>{entry.getIn(['data', 'title'])}</h1>
-          <div dangerouslySetInnerHTML={{ __html: widgetFor('body')}} />
-        </Col>
-      </Row>
-    </section>
-  </div>
-);
+      <section className="container">
+        <Row>
+          <Col>
+            <h1>{entry.getIn(['data', 'title'])}</h1>
+            <div dangerouslySetInnerHTML={{ __html: widgetFor('body')}} />
+          </Col>
+        </Row>
+      </section>
+    </div>
+  );
+}
 
 export default CustomsPreview;
