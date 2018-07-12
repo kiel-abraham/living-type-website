d063685343cc3cdb8ff442bee859e242a10b6113
kiel-abraham <kiel.abraham@icloud.com>
2018-07-12 16:59:06 +1000

    Header CMS

--- src/cms/preview-templates/header-preview.js
+++ src/cms/preview-templates/header-preview.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import Header from '../../components/header';
+import { Nav, NavbarBrand, Link } from 'reactstrap';
 
-const HeaderPreview = ({ entry, widgetFor }) => (
-    <Header style={entry.getIn(['data', 'headerStyle'])} />
-);
+// will need to use gif images to display how the menu would work
+
+const HeaderPreview = ({ entry, widgetFor }) => {
+    if (entry.getIn(['data', 'headerStyle']) === "HeaderSlide") {
+        return(
+            <div>
+                <h3>Slide</h3>
+                <nav className="navbar navbar-expand-md navbar-dark bg-primary">Test</nav>
+                <img src="http://gph.is/2c2CSvz" />
+            </div>
+        );
+    } else {
+        return(
+            <div>
+                <h3>Other</h3>
+                <img src="http://gph.is/2c82eID" />
+            </div>
+        ); 
+    }
+}
 
 export default HeaderPreview;
