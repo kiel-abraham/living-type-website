3923ced5e90daaca8317d1104ceb3fcea842da19
kiel-abraham <kiel.abraham@icloud.com>
2018-07-14 11:09:28 +1000

    Contact CMS testing

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,13 @@
 import CMS from 'netlify-cms';
 
+import ContactPreview from './preview-templates/contact-preview';
 import CustomsPreview from './preview-templates/customs-preview';
 import HeaderPreview from './preview-templates/header-preview';
 import FooterPreview from './preview-templates/footer-preview';
 
 
 CMS.registerPreviewStyle('https://bootswatch.com/4/materia/bootstrap.min.css');
+CMS.registerPreviewTemplate('contact', ContactPreview);
 CMS.registerPreviewTemplate('customs', CustomsPreview);
 CMS.registerPreviewTemplate('header', HeaderPreview);
 CMS.registerPreviewTemplate('footer', FooterPreview);
