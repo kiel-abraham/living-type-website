53c914c218ccb7882249b3cabbbff20a20a9554d
kiel-abraham <kiel.abraham@icloud.com>
2018-07-20 19:46:51 +1000

    Contact CMS

--- src/cms/preview-templates/contact-preview.js
+++ src/cms/preview-templates/contact-preview.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import Banner from '../../components/banner';
+import { Row, Col } from 'reactstrap';
 
 const ContactPreview = ({ entry, widgetFor }) => {
-    const showHero = entry.getIn(['data', 'hero', 'showHero']);
-    const heroFull = entry.getIn(['data', 'hero', 'heroFull']);
-    const heroTitle = entry.getIn(['data', 'hero', 'heroTitle']);
-    return(
-  <div>
-    <Banner
-      showHero={showHero}
-      heroFull={heroFull}
-      heroTitle={heroTitle}
-      />
-  </div>
-);
+    <section className="container">
+      <Row>
+        <Col>
+          <h1>{entry.getIn(['data', 'title'])}</h1>
+          <div>{widgetFor('body')}</div>
+        </Col>
+      </Row>
+    </section>
 }
 
 export default ContactPreview;
--- src/pages/default/contact.md
+++ src/pages/default/contact.md
@@ -4,9 +4,5 @@ pageType: "contact"
 slug: "/contact"
 nav: true
 navSort: 4
-hero:
-  showHero: true
-  heroFull: false
-  heroTitle: "Get In Touch"
 ---
 This is the contact page.
