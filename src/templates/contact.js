6d13c6fd63b47cf708f66173fac14a62905743bb
kiel-abraham <kiel.abraham@icloud.com>
2018-08-16 08:50:17 +1000

    Working on contact page

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { Row, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import FaPhone from 'react-icons/lib/fa/phone';
+import FaEmail from 'react-icons/lib/fa/envelope';
+import FaHome from 'react-icons/lib/fa/home';
 
 const ContactPage = ({ data }) => {
   const frontmatter = data.markdownRemark.frontmatter;
@@ -10,11 +13,20 @@ return (
 
     <section className="container mt-4">
       <Row>
-        <Col>
+        <Col xs="12">
           <h1>{frontmatter.title}</h1>
+        </Col>
+        <Col sm="6">
           <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-
+          <address>
+            <FaPhone/> <a href={`tel:${data.markdownRemark.frontmatter.phone}`}>{data.markdownRemark.frontmatter.phone}</a><br/>
+            <FaEmail/> <a href={`mailto:${data.markdownRemark.frontmatter.email}`}>{data.markdownRemark.frontmatter.email}</a><br/>
+            <FaHome/> {data.markdownRemark.frontmatter.address}
+          </address>
+        </Col>
+        <Col sm="6">
         <Form>
+          <h2>Send a message</h2>
           <FormGroup>
           <Label for="name">Name</Label>
           <Input type="text" name="name" id="name" placeholder="Enter your name" />
@@ -49,6 +61,9 @@ export const ContactQuery = graphql`
       html
       frontmatter {
         title
+        phone
+        email
+        address
       }
     }
   }
