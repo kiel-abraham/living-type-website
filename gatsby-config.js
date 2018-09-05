6172748ae29bb370583d45b67c68b45c72998437
kiel-abraham <kiel.abraham@icloud.com>
2018-09-05 20:59:07 +1000

    Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,8 +49,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-        htmlTitle: 'THKR CMS'
+        modulePath: `${__dirname}/src/cms/cms.js`
       }
     },
     'gatsby-plugin-netlify'
