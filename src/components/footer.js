6051314f830e0cca2070fcdeed2d706fe450231d
kiel-abraham <kiel.abraham@icloud.com>
2018-09-15 21:38:11 +1000

    Footer link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'gatsby-link';
 import FaFacebook from 'react-icons/lib/fa/facebook-square';
 import FaGoogle from 'react-icons/lib/fa/google-plus-square';
 import FaTwitter from 'react-icons/lib/fa/twitter-square';
@@ -86,7 +85,7 @@ const Footer = ({ background, socialColour, links, siteTitle, siteUrl }) => {
         })}
       </ul>
       <p className={`text-${copyColour}`}>&copy; {year} {siteTitle}</p>
-      <small className="text-muted">Powered by <Link to="//www.thkr.com.au">THKR</Link></small>
+      <small className="text-muted">Powered by <a href="https//www.thkr.com.au" target="_blank">THKR</a></small>
     </footer>
   );
 }
