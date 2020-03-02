ea926285124751aaea75b581e722a5e303448041
Kiel Abraham <32699432+kiel-abraham@users.noreply.github.com>
2020-03-03 07:34:22 +1000

    Remove Messenger chat

--- src/components/layout.js
+++ src/components/layout.js
@@ -106,34 +106,6 @@ const Layout = ({ children, data }) => (
         {children}
       </main>
 
-      <!-- Load Facebook SDK for JavaScript -->
-      <div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v4.0'
-          });
-        };
-
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
-
-      <!-- Your customer chat code -->
-      <div class="fb-customerchat"
-        attribution=setup_tool
-        page_id="1155395824582092"
-  theme_color="#f0ad4e"
-  greeting_dialog_display="hide"
-  logged_in_greeting="Hi! Get in touch with us"
-  logged_out_greeting="Hi! Get in touch with us">
-      </div>
-
       <Footer
         background={data.settings.frontmatter.footerBackground}
         socialColour={data.settings.frontmatter.socialColour}
